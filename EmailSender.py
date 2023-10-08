7c6f7a74744037dd585399c43605791165e73819
Amirhossein Moznebi <59997964+amirimoo@users.noreply.github.com>
2023-10-08 14:42:18 +0330

    Set Email and Password

---
+++ EmailSender.py
@@ -0,0 +1,9 @@
+from email.message import EmailMessage
+from Password      import password
+
+#Start Set Email and Password
+ 
+Email_Sender   = "exampleemailsender@gmail.com"
+Email_Password = password
+
+#Finish Set Email and Password
